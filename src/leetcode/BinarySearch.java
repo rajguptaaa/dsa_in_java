819c73ee736ead0ccbb3a561d70086ea74ede59f
Raj Gupta <kumaaraj11@gmail.com>
2026-02-24 09:41:27 +0530

    initial commit

---
+++ src/leetcode/BinarySearch.java
@@ -0,0 +1,22 @@
+package leetcode;
+public class BinarySearch {
+    public static int search(int[] arr, int k){
+        int low = 0;
+        int high = arr.length - 1;
+        while(low <= high){
+            int mid = low + (high - low) / 2;
+            if(arr[mid] == k){
+                return mid;
+            } else if(arr[mid] > k){
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args){
+        int arr[] = {1,2,3,4,5,6,7,8,9,10};
+        System.out.println(BinarySearch.search(arr, 4));
+    }
+}
